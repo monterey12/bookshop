e800c5faafa1a5988d282158480155f21cb4b8f8
monterey12 <92842939+monterey12@users.noreply.github.com>
2021-12-01 19:44:32 +0900

    Initial commit

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+//    @RequestMapping(method = RequestMethod.POST, path = "/postMethod")
+    @PostMapping(path = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam) {
+
+        return searchParam;
+
+    }
+
+    @PutMapping("/putMethod")
+    public void put(){
+
+    }
+
+    @PatchMapping("/patchMethod")
+    public void patch(){
+
+    }
+
+    @DeleteMapping("/deleteMethod")
+    public void delete(){
+
+    }
+}
